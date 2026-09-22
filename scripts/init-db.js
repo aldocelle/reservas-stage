582021a390abfc146f1a92832ddfcf3dda1f168b
aldocelle <celle.aldo@gmail.com>
2026-09-22 17:36:52 -0300

    refactor: retirar backend Node/PostgreSQL reemplazado por PHP/MySQL

--- scripts/init-db.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs/promises';
-import { pool } from '../db.js';
-const sql=await fs.readFile(new URL('../schema.sql',import.meta.url),'utf8');
-await pool.query(sql);
-console.log('Base de datos inicializada.');
-await pool.end();
